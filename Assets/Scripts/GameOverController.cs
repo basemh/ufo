ed209fba078f4f91ace1768b8b6088af7d1867a3
Basem Hegazy <basem.hegazy@icloud.com>
2017-08-11 15:48:53 +0200

    master project repository
    master project repository.

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		playAgain ();
+	}
+
+	void playAgain () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene("Main");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+
+	private Rigidbody2D rb2d;
+	private int count;
+
+	void Start () {
+
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		setCountText ();
+	}
+
+	/*
+	void Update () {
+		playAgain ();
+	}
+	*/
+
+	void FixedUpdate () {
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+
+		if (other.gameObject.CompareTag ("PickUp")) {
+
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			setCountText ();
+		}
+	}
+
+	void setCountText () {
+		
+		countText.text = count.ToString ();
+		if (count >= 17){
+			SceneManager.LoadScene("GameOver");
+		} 
+	}
+
+}
